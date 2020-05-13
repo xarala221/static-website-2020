9020c5841cdf90e6d71cedb20b1df66122352da0
Ousseynou Diop <xaralaxarala@gmail.com>
2020-05-13 12:04:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# static-website-2020
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    const date = new Date();
+    $('.year').text = date.getFullYear();
+
+})
+$(window).on('load', function () { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
